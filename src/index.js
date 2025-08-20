0e24d3c3cb8ba2b80ce5e076da9edb3c103b0c99
Awaken Developer <awakenx.dev@gmail.com>
2025-08-20 22:58:47 +0200

    Initial working bot setup.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+});
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
